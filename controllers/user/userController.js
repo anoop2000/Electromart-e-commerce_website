ff377cda03ea29a0e546eb5f556d838e31ba3708
anoop2000 <88547121+anoop2000@users.noreply.github.com>
2024-11-09 00:30:00 +0530

    project initialized

---
+++ controllers/user/userController.js
@@ -0,0 +1,24 @@
+
+
+const pageNotFound = async (req,res)=>{
+    try {
+         res.render("page-404")
+    } catch (error) {
+        res.rediret("/pageNotFound")
+    }
+}
+
+
+
+const loadHomePage = async(req,res)=>{
+    try {
+        return res.render("home")
+        
+    } catch (error) {
+        console.log("Home page not found");
+        res.status(500).send("Server error")
+        
+    }
+}
+
+module.exports = {loadHomePage,pageNotFound}
